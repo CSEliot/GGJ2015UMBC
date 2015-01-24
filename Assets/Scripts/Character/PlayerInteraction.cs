cb50261535bef7b884df00e92029643bdf1842f5
Austin <apagano1@umbc.edu>
2015-01-24 02:33:04 -0500

    character controller done, interaction controller done

---
+++ Assets/Scripts/Character/PlayerInteraction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteraction : MonoBehaviour {
+
+	Ray rayOrigin;
+	RaycastHit hitInfo;
+	Vector3 rayOriginStart;
+	bool ableToInteract;
+
+	public float reach;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+
+		rayOriginStart = transform.FindChild ("Main Camera").transform.position / transform.FindChild ("Main Camera").transform.position.magnitude;
+		
+		rayOrigin = new Ray(transform.FindChild("Main Camera").transform.position + transform.FindChild("Main Camera").transform.forward * 0.05f, transform.FindChild("Main Camera").transform.forward);
+
+		if (Physics.Raycast (rayOrigin, out hitInfo, reach)) {
+			if (hitInfo.transform.tag == "table") {
+				ableToInteract = true;
+				Debug.Log("hit table");
+			}
+		}
+	}
+}
