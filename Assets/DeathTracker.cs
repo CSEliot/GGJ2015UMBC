45b467ad975caeb22b5cd6e90bd590cee287f5f4
Eliot <ecsit39@gmail.com>
2015-01-24 13:58:30 -0500

    Deading player

---
+++ Assets/DeathTracker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTracker : MonoBehaviour {
+
+
+	private int totalDeaths;
+
+	// Use this for initialization
+	void Start () {
+		totalDeaths = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		
+	}
+
+	public void increaseDeathCount(){
+		totalDeaths += 1;
+	}
+
+	public int getDeathCount(){
+		return totalDeaths;
+	}
+}
