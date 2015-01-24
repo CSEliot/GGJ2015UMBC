da5a1f39adb527cd7a4036815408ffd1117f10f5
Austin <apagano1@umbc.edu>
2015-01-24 03:50:12 -0500

    made a cross hair that lights up to intractable things

--- Assets/Scripts/Character/PlayerInteraction.cs
+++ Assets/Scripts/Character/PlayerInteraction.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class PlayerInteraction : MonoBehaviour {
@@ -8,27 +10,43 @@ public class PlayerInteraction : MonoBehaviour {
 	Vector3 rayOriginStart;
 	bool ableToInteract;
 
+	public Button crossHair;
 	public float reach;
 
 	// Use this for initialization
 	void Start () {
-	
+		Screen.lockCursor = true;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
+		//make a pointer to trigger events for the ui without needing to move the mouse around
+		PointerEventData pointer = new PointerEventData(EventSystem.current);
 
 
 		rayOriginStart = transform.FindChild ("Main Camera").transform.position / transform.FindChild ("Main Camera").transform.position.magnitude;
 		
 		rayOrigin = new Ray(transform.FindChild("Main Camera").transform.position + transform.FindChild("Main Camera").transform.forward * 0.05f, transform.FindChild("Main Camera").transform.forward);
 
+		//if you are able to reach something, anything important or not
 		if (Physics.Raycast (rayOrigin, out hitInfo, reach)) {
+
+			//every if statement will now check for whether we hit an interactable object or not
 			if (hitInfo.transform.tag == "table") {
 				ableToInteract = true;
-				Debug.Log("hit table");
 			}
 		}
+
+		//highlight crosshair to signal possible interaction
+		if (ableToInteract) {
+			ExecuteEvents.Execute(crossHair.gameObject, pointer, ExecuteEvents.pointerEnterHandler);
+		}
+		if (!ableToInteract) {
+			ExecuteEvents.Execute(crossHair.gameObject, pointer, ExecuteEvents.pointerExitHandler);		
+		}
+
+		//end update with disabling interaction
+		ableToInteract = false;
 	}
 }
