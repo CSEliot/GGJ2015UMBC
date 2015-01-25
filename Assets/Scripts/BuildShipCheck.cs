26db44145ca37d9a0239a22e5187c19d552f156e
Austin <apagano1@umbc.edu>
2015-01-25 14:44:11 -0500

    austin

--- Assets/Scripts/BuildShipCheck.cs
+++ Assets/Scripts/BuildShipCheck.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class BuildShipCheck : MonoBehaviour {
 
+	public AudioClip buildingShip;
+
 	//set up a list to find the 5 ship pieces
 	public GameObject part1;
 	public GameObject part2;
 	public GameObject part3;
 	public GameObject part4;
 	public GameObject part5;
-	
+
+	public GameObject ship;
+	private bool shipSpawned = false;
+
 	//the average distance between all other parts relative to part 1
 	private float avgDistance;
 	
@@ -62,9 +67,24 @@ public class BuildShipCheck : MonoBehaviour {
 		distanceSum = distance1to2 + distance1to3 + distance1to4 + distance1to5;
 		avgDistance = distanceSum / 4f;
 		
-		if (avgDistance < 4f) {
+		if (avgDistance < 4f && !GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>().getIsCarrying() && !shipSpawned) {
 			//assemble ship player wins
 			Debug.Log("player wins and builds ship");
+
+			part1.SetActive(false);
+			part2.SetActive(false);
+			part3.SetActive(false);
+			part4.SetActive(false);
+			part5.SetActive(false);
+
+			Vector3 whereToSpawn;
+
+			whereToSpawn = new Vector3(part1.transform.position.x, part1.transform.position.y + 10f, part1.transform.position.z);
+			GameObject newShip;
+			newShip = Instantiate(ship, whereToSpawn, Quaternion.Euler(-90f, -180f, -180f)) as GameObject;
+			shipSpawned = true;
+
+			audio.PlayOneShot(buildingShip);
 		}
 	}
 }
