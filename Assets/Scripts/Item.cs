45806b1ed499b792a691ec5939c9d7e01660cef5
Eliot <ecsit39@gmail.com>
2015-01-24 23:34:10 -0500

    PITS MOTHERFUCKER

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	public int Wieght =  5; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		//make sure that this object is tagged with as an item
+		this.tag = "Item"; 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
